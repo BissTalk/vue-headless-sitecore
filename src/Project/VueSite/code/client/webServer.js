3655a4274e24091f88ddb388d7f16dd0172edf13
Bissol, Joseph <Joseph_Bissol@cable.comcast.com>
2018-03-12 13:51:06 -0400

    Add yarn/npm run scripts

--- src/Project/VueSite/code/client/webServer.js
+++ src/Project/VueSite/code/client/webServer.js
@@ -1,4 +1,5 @@
-﻿const LocalWebServer = require("local-web-server");
+﻿"use strict";
+const LocalWebServer = require("local-web-server");
 const localWebServer = new LocalWebServer();
 
 var yarn = "yarn";
@@ -7,35 +8,36 @@ if (process.platform === "win32") {
     yarn = "yarn.cmd";
 }
 
-var spawn = require("child_process").spawn;
-spawn(yarn, ["run", "build"], { stdio: "inherit" });
+const spawn = require("child_process").spawn;
+const build = spawn(yarn, ["run", "build"], { stdio: "inherit" });
 
-const server = localWebServer.listen({
-    port: 3001,
-    directory: "wwwroot",
-    spa: "index.html",
-    rewrite: [{ from: "/-/*", to: "http://sc812.local/-/$1" },
-        { from: "/headless/*", to: "http://sc812.local/headless/$1" }
-     ]
+build.on("close", (code) =>
+{
+    if (code === 0) {
+        const server = localWebServer.listen({
+            port: 3001,
+            directory: "wwwroot",
+            spa: "index.html",
+            rewrite: [{ from: "/-/*", to: "http://sc812.local/-/$1" },
+                { from: "/headless/*", to: "http://sc812.local/headless/$1" }
+             ]
+        });
+        if (process.platform === "win32") {
+            const rl = require("readline").createInterface({
+                input: process.stdin,
+                output: process.stdout
+            });
+
+            rl.on("SIGINT", function () {
+                process.emit("SIGINT");
+            });
+        }
+
+        process.on("SIGINT", function () {
+            //graceful shutdown
+            server.close();
+            process.exit();
+        });
+        console.log("\nTo exit (Crtl-C)");
+    }
 });
-
-if (process.platform === "win32") {
-    var rl = require("readline").createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-
-    rl.on("SIGINT", function () {
-        process.emit("SIGINT");
-    });
-}
-
-process.on("SIGINT", function () {
-    //graceful shutdown
-    server.close();
-    process.exit();
-});
-
-//server.close();
-
-console.log("\nTo exit (Crtl-C)")
